081cdc179f3ac510e452a8837819954970ff9d8d
Adrian Reyes <sadrian.reyes@gmail.com>
2016-09-26 21:46:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# travis-sonar-gradle
+A simple test
---
+++ src/test/java/com/pistachosoft/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.pistachosoft;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CalculatorTest {
+
+    private Calculator sut;
+
+    @Before
+    public void setUp() {
+        sut = new Calculator();
+    }
+
+    @Test
+    public void add() {
+        int result = sut.add(2, 3);
+
+        assertThat(result, is(5));
+    }
+}
